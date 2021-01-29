f257ea772e579a8f455d82c17278e02d20c22625
seungkey <61777003+seungkey@users.noreply.github.com>
2021-01-29 21:20:41 +0900

    Create (level1)같은수는싫어.py
    (level1)같은 수는 싫어

---
+++ (level1)같은수는싫어.py
@@ -0,0 +1,8 @@
+def solution(arr):
+    answer = []
+    for i in range(len(arr)-1):
+        if(arr[i]!=arr[i+1]):
+            answer.append(arr[i])
+        if(i+1==len(arr)-1):
+            answer.append(arr[i+1])
+    return answer
