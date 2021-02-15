434eb472e3021bacad0c47504d6addd126c24ea7
seungkey <61777003+seungkey@users.noreply.github.com>
2021-02-15 17:24:33 +0900

    Update (level2)스킬트리.py
    런타임 에러 수정

--- (level2)스킬트리.py
+++ (level2)스킬트리.py
@@ -2,13 +2,15 @@ def solution(skill, skill_trees):
     answer = 0
     for i in skill_trees:
         a=[]
+        correct=True
         for j in i:
-            for k in skill:
-                if(j==k):
-                    a.append(j)
-        a=''.join(a)
-        if(skill[0]==a[0]):
-            if(a in skill):
-                answer+=1
+            if j in skill:
+                a.append(j)
+        for k in range(len(a)):
+            if a[k]!=skill[k]:
+                correct=False
+                break
+        if correct==True:
+            answer+=1
         
     return answer
